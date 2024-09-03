3b368024277276c8426e53f8a1f9b1a491067fc9
Mohammed-aqhib <mohammedaqhib2106@gmail.com>
2024-09-03 16:23:36 +0530

    api

--- express/main.js
+++ express/main.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var app = express();
+app.use(express.json());
 app.get("/myname", (req, res) => {
-    res.json({"msg" : "Jofin"});
+    res.json({"msg" : "mass Jofin"});
+});
+
+app.post("/myname", (req, res) => {
+    res.json({"msg" : "mass Aqhib"});
 });
 
 app.listen(8080, () => {
     console.log('server started')
 })
+app.post("/login",(request, response) =>{
+    let email = request["query"]["email"];
+    let pass = request["query"]["password"];
+
+    if (email == "admin@gmail.com" && pass == "admin"){
+        response.json({ message: "login successfull" });
+    } else {
+        response.json({ message: "credentials failed" });
+    }
+
+    // response.json({ email: email, password: pass });
+});
+
+app.post("/register", (request, response) => {
+    let { name, email, password, address } = request.body;
+    if (!name || !email || !password || !address){
+        response.json({ message: "enter all the details"});
+    }else {
+        response.json({ message: "register successfully"});
+    }
+});
+app.post("/add", (request, response) => {
+  let { num1, num2 } = request.body;
+  if (!num1 || !num2) {
+    response.json({ message: "Enter 2 Values" });
+  } else {
+    response.json({ value: num1 + num2 });
+  }
+}); 
