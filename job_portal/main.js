2b064d1db0129115637523b20e034d64436ade9d
Mohammed-aqhib <mohammedaqhib2106@gmail.com>
2024-09-13 16:04:11 +0530

    token

--- job_portal/main.js
+++ job_portal/main.js
@@ -1,20 +1,28 @@
 var express = require("express");
 var app = express();
-var cors = require("cors");
 app.use(express.json());
-app.use(cors());
-const { MongoClient, ObjectId } = require("mongodb");
 
-const url = "mongodb+srv://ashokgtncollege:Kumar9840@cluster0.ij4i5.mongodb.net/";
+const { MongoClient, ObjectId } = require("mongodb");
+var cors = require("cors");
+app.use(cors());
+
+app.use((req, res, next) => {
+
+
+  console.log("i'm  a Middleware");
+  next();
+});
+
+const url = "mongodb+srv://karankumar:karan2909@cluster0.viptk.mongodb.net/";
 const client = new MongoClient(url);
 
 const dbName = "jobportal";
 
-app.post("/create_jobs", async (req, res) => {
+app.post("/api/create_jobs", async (req, res) => {
   let body = req.body;
   let data = {
     name: body["name"],
-    comapny_name: body["company_name"],
+    company_name: body["company_name"],
     requirements: body["requirements"],
   };
   await client.connect();
@@ -24,32 +32,76 @@ app.post("/create_jobs", async (req, res) => {
   res.json({ msg: "Successfully created a job" });
 });
 
-app.get("/list_jobs", async (req, res) => {
+app.get("/api/list_jobs", async (req, res) => {
   await client.connect();
   let db = client.db("office");
   let data = await db.collection("jobportal").find({}).toArray();
   res.json(data);
 });
 
-app.put("/update_job", async (req, res) => {
-  let { name, requirements } = req.body;
+// app.put("/update_job", async (req, res) => {
+//   let { name, requirements } = req.body;
+//   await client.connect();
+//   let db = client.db("office");
+//   await db
+//     .collection("jobportal")
+//     .updateOne({ name: name }, { $set: { requirements: requirements } });
+
+//   res.json({ msg: "Updated the requirements" });
+// });
+
+app.put("/api/update_job", async (req, res) => {
+  let { id, name, company, requirements } = req.body;
   await client.connect();
   let db = client.db("office");
-  await db
-    .collection("jobportal")
-    .updateOne({ name: name }, { $set: { requirements: requirements } });
+  await db.collection("jobportal").updateOne(
+    { _id: new ObjectId(id) },
+    {
+      $set: { requirements: requirements, name: name, company_name: company },
+    }
+  );
 
   res.json({ msg: "Updated the requirements" });
 });
 
-app.delete("/delete_job", async (req, res) => {
-  let { name } = req.query;
+app.delete("/api/delete_job", async (req, res) => {
+  let { id } = req.query;
   await client.connect();
   let db = client.db("office");
-  await db.collection("jobportal").deleteOne({ name: name });
+  await db.collection("jobportal").deleteOne({ _id: new ObjectId(id) });
 
   res.json({ msg: "Deleted the Job" });
 });
 
+var jwt = require("jsonwebtoken");
+
+app.post("/login", async (req, res) => {
+  await client.connect();
+  let { name, password } = req.body;
+  let db = client.db("office");
+  const teachers = await db
+    .collection("teachers")
+    .find({ name: name, password: password })
+    .toArray();
+  if (teachers.length > 0) {
+    var token = jwt.sign({ name: teachers[0]["name"] }, "SECRET");
+    res.json({ message: "Login Successful", token: token });
+  } else {
+    res.status(401).json({ error: "Password incorrect" });
+  }
+});
+
+app.post("/register", async (req, res) => {
+  let body = req.body;
+  let data = {
+    name: body["name"],
+    email: body["email"],
+    password: body["password"],
+  };
+  await client.connect();
+  let db = client.db("office");
+  await db.collection("teachers").insertOne(data);
+  res.status(200).json({ message: "Created New Teacher Record!!" });
+});
 app.listen(8080);
 
